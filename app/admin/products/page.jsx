7b08b475e5a0be335542637f4d2ab04fbbfe5fd9
developeritexpert <developeritexpert3@gmail.com>
2025-11-24 16:35:32 +0530

    24 nov changes

--- app/admin/products/page.jsx
+++ app/admin/products/page.jsx
@@ -1,5 +1,4 @@
 "use client"
-import CustomSelect from "@/app/Components/layout/CustomSelect";
 import { useState } from "react";
 
 function Page() {
@@ -16,16 +15,16 @@ function Page() {
   ];
 
   const tableData = [
-    { id: 1, productName: "2019 Topos Supreme Quad Autographs", price: "$18,000", sku: "AT234", stock: "120", status: "In Stock", action: "Edit | Remove" },
-    { id: 2, productName: "2019 Topos Heritage Real One Autographs", price: "$15,517", sku: "N5678", stock: "0", status: "Out of Stock", action: "Edit | Remove" },
-    { id: 3, productName: "2005 SP Signature Autograph Interedible Inscription", price: "$2,760", sku: "S9801", stock: "20", status: "Re-stocking", action: "Edit | Remove" },
-    { id: 4, productName: "2019 Ramin One and One Roskie Jersey Autograph", price: "$2,249", sku: "L4321", stock: "78", status: "In Stock", action: "Edit | Remove" },
-    { id: 5, productName: "2019 Domuss Optic Roskie Signature Fast Break Gold Pfrom", price: "$2,000", sku: "L8765", stock: "55", status: "In Stock", action: "Edit | Remove" },
-    { id: 6, productName: "2019 Ramin One and One Roskie Dual Jersey Autographs", price: "$1,149", sku: "S2222", stock: "0", status: "Out of Stock", action: "Edit | Remove" },
-    { id: 7, productName: "2000 Bowman Chrome", price: "$1,098", sku: "C9001", stock: "48", status: "In Stock", action: "Edit | Remove" },
-    { id: 8, productName: "2018 Ramin Domuss Prost Prost Black Laser", price: "$2,000", sku: "A9679", stock: "10", status: "Re-stocking", action: "Edit | Remove" },
-    { id: 9, productName: "2017 Ramin Contenters' Horizontal Variation Roskie Ticket", price: "$989", sku: "M1357", stock: "0", status: "Out of Stock", action: "Edit | Remove" },
-    { id: 10, productName: "2009 SP Game Used Signature Fairtics", price: "$989", sku: "K4410", stock: "12", status: "In Stock", action: "Edit | Remove" }
+    { id: 1, productName: "2019 Topos Supreme Quad Autographs", price: "$18,000", sku: "AT234", stock: "120", status: "In Stock", image: "/img/product1.png" },
+    { id: 2, productName: "2019 Topos Heritage Real One Autographs", price: "$15,517", sku: "N5678", stock: "0", status: "Out of Stock", image: "/img/product2.png" },
+    { id: 3, productName: "2005 SP Signature Autograph Interedible Inscription", price: "$2,760", sku: "S9801", stock: "20", status: "Re-stocking", image: "/img/product3.png" },
+    { id: 4, productName: "2019 Ramin One and One Roskie Jersey Autograph", price: "$2,249", sku: "L4321", stock: "78", status: "In Stock", image: "/img/product4.png" },
+    { id: 5, productName: "2019 Domuss Optic Roskie Signature Fast Break Gold Pfrom", price: "$2,000", sku: "L8765", stock: "55", status: "In Stock", image: "/img/product5.png" },
+    { id: 6, productName: "2019 Ramin One and One Roskie Dual Jersey Autographs", price: "$1,149", sku: "S2222", stock: "0", status: "Out of Stock", image: "/img/product6.png" },
+    { id: 7, productName: "2000 Bowman Chrome", price: "$1,098", sku: "C9001", stock: "48", status: "In Stock", image: "/img/product7.png" },
+    { id: 8, productName: "2018 Ramin Domuss Prost Prost Black Laser", price: "$2,000", sku: "A9679", stock: "10", status: "Re-stocking", image: "/img/product8.png" },
+    { id: 9, productName: "2017 Ramin Contenters' Horizontal Variation Roskie Ticket", price: "$989", sku: "M1357", stock: "0", status: "Out of Stock", image: "/img/product9.png" },
+    { id: 10, productName: "2009 SP Game Used Signature Fairtics", price: "$989", sku: "K4410", stock: "12", status: "In Stock", image: "/img/product10.png" }
   ];
 
   const getStatusColor = (status) => {
@@ -37,12 +36,14 @@ function Page() {
     }
   };
 
-  const getActionButton = (action) => {
-    return (
-      <span className="tborder border-[#F7F8F81C] bg-[#F7F8F80A] rounded-[7px] py-[6px] px-[15px] text-xs whitespace-nowrap hover:bg-[#F7F8F815] transition-colors cursor-pointer">
-        {action}
-      </span>
-    );
+  const handleEdit = (id) => {
+    console.log('Edit product:', id);
+    // Add your edit logic here
+  };
+
+  const handleRemove = (id) => {
+    console.log('Remove product:', id);
+    // Add your remove logic here
   };
 
   return (
@@ -64,7 +65,7 @@ function Page() {
           <button
             className="flex gap-[5px] items-center bg-gradient-to-r from-[#75DA5B] to-[#4DCE94] rounded-[7px] text-black font-semibold px-[20px] py-[10px] text-sm">
             <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M7.62402 0.255859C7.91001 0.284359 8.17878 0.411199 8.38379 0.616211C8.61821 0.850631 8.75 1.16848 8.75 1.5V6.25H13.5C13.8315 6.25 14.1494 6.38179 14.3838 6.61621C14.6182 6.85063 14.75 7.16848 14.75 7.5C14.75 7.83152 14.6182 8.14937 14.3838 8.38379C14.1494 8.61821 13.8315 8.75 13.5 8.75H8.75V13.5C8.75 13.8315 8.61821 14.1494 8.38379 14.3838C8.14937 14.6182 7.83152 14.75 7.5 14.75C7.16848 14.75 6.85063 14.6182 6.61621 14.3838C6.38179 14.1494 6.25 13.8315 6.25 13.5V8.75H1.5C1.16848 8.75 0.850631 8.61821 0.616211 8.38379C0.381791 8.14937 0.25 7.83152 0.25 7.5C0.25 7.16848 0.381791 6.85063 0.616211 6.61621C0.850631 6.38179 1.16848 6.25 1.5 6.25H6.25V1.5C6.25 1.16848 6.38179 0.850631 6.61621 0.616211C6.85063 0.38179 7.16848 0.25 7.5 0.25L7.62402 0.255859Z" fill="black" stroke="black" stroke-width="0.5" />
+              <path d="M7.62402 0.255859C7.91001 0.284359 8.17878 0.411199 8.38379 0.616211C8.61821 0.850631 8.75 1.16848 8.75 1.5V6.25H13.5C13.8315 6.25 14.1494 6.38179 14.3838 6.61621C14.6182 6.85063 14.75 7.16848 14.75 7.5C14.75 7.83152 14.6182 8.14937 14.3838 8.38379C14.1494 8.61821 13.8315 8.75 13.5 8.75H8.75V13.5C8.75 13.8315 8.61821 14.1494 8.38379 14.3838C8.14937 14.6182 7.83152 14.75 7.5 14.75C7.16848 14.75 6.85063 14.6182 6.61621 14.3838C6.38179 14.1494 6.25 13.8315 6.25 13.5V8.75H1.5C1.16848 8.75 0.850631 8.61821 0.616211 8.38379C0.381791 8.14937 0.25 7.83152 0.25 7.5C0.25 7.16848 0.381791 6.85063 0.616211 6.61621C0.850631 6.38179 1.16848 6.25 1.5 6.25H6.25V1.5C6.25 1.16848 6.38179 0.850631 6.61621 0.616211C6.85063 0.38179 7.16848 0.25 7.5 0.25L7.62402 0.255859Z" fill="black" stroke="black" strokeWidth="0.5" />
             </svg>
             Add Product
           </button>
@@ -85,17 +86,41 @@ function Page() {
           <tbody>
             {tableData.map((item) => (
               <tr key={item.id} className='text-[#F7F8F8B2] bg-[#F7F8F81C] hover:bg-[#F7F8F82A] transition-colors'>
-                <td className='py-[15px] px-[15px] border border-[#F7F8F81C]'>{item.productName}</td>
-                <td className='py-[15px] px-[15px] border border-[#F7F8F81C]'>{item.price}</td>
-                <td className='py-[15px] px-[15px] border border-[#F7F8F81C]'>{item.sku}</td>
-                <td className='py-[15px] px-[15px] border border-[#F7F8F81C]'>{item.stock}</td>
-                <td className='py-[15px] px-[15px] border border-[#F7F8F81C]'>
+                <td className='py-[10px] px-[15px] border border-[#F7F8F81C]'>
+                  <div className="flex items-center gap-3">
+                    <div className="border border-[#F7F8F81C] rounded-[7px] p-2">
+                      <img 
+                        src={item.image} 
+                        alt={item.productName}
+                        className="w-10 h-10 rounded-[7px] object-contain"
+                      />
+                    </div>
+                    <span>{item.productName}</span>
+                  </div>
+                </td>
+                <td className='py-[10px] px-[15px] border border-[#F7F8F81C]'>{item.price}</td>
+                <td className='py-[10px] px-[15px] border border-[#F7F8F81C]'>{item.sku}</td>
+                <td className='py-[10px] px-[15px] border border-[#F7F8F81C]'>{item.stock}</td>
+                <td className='py-[10px] px-[15px] border border-[#F7F8F81C]'>
                   <span className={getStatusColor(item.status)}>
                     {item.status}
                   </span>
                 </td>
-                <td className='py-[15px] px-[15px] border border-[#F7F8F81C]'>
-                  {getActionButton(item.action)}
+                <td className='py-[10px] px-[15px] border border-[#F7F8F81C]'>
+                  <div className="flex gap-2">
+                    <button 
+                      onClick={() => handleEdit(item.id)}
+                      className="border border-[#F7F8F81C] bg-[#F7F8F80A] rounded-[7px] py-[6px] px-[15px] text-xs whitespace-nowrap hover:bg-[#F7F8F815] transition-colors cursor-pointer"
+                    >
+                      Edit
+                    </button>
+                    <button 
+                      onClick={() => handleRemove(item.id)}
+                      className="border border-[#F7F8F81C] bg-[#F7F8F80A] rounded-[7px] py-[6px] px-[15px] text-xs whitespace-nowrap hover:bg-[#F7F8F815] transition-colors cursor-pointer"
+                    >
+                      Remove
+                    </button>
+                  </div>
                 </td>
               </tr>
             ))}
